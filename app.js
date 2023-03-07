9c73c35cf813e989f4a51547140c619e219dc36b
evdokim-mellin <evdockimpyatkov.ru@yandex.ru>
2023-03-07 08:08:21 +0300

    Revert "fix: 5"
    This reverts commit 1f7d4993aa8b21f2c5b7cdfcaaf26df7997fecbc.

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const { celebrate, Joi } = require('celebrate');
 
 const app = express();
 
-// const userRouter = require('./routes/users');
-// const movieRouter = require('./routes/movies');
+const userRouter = require('./routes/users');
+const movieRouter = require('./routes/movies');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-// const { login, createUser, signOut } = require('./controllers/users');
-// const auth = require('./middlewares/auth');
+const { login, createUser, signOut } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000, DATA_BASE = 'mongodb://localhost:27017/movies-explorer-db' } = process.env;
@@ -62,21 +62,21 @@ app.post('/signin', celebrate({
     password: Joi.string().required(),
   }),
 }), (req, res, next) => { res.send({ message: 'asdasd' }); });
-// app.post('/signup', celebrate({
-//   body: Joi.object().keys({
-//     email: Joi.string().email().required(),
-//     password: Joi.string().required(),
-//     name: Joi.string().min(2).max(30).required(),
-//   }),
-// }), createUser);
-// app.post('/signout', signOut);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30).required(),
+  }),
+}), createUser);
+app.post('/signout', signOut);
 
 app.use(requestLogger);
 
-// app.use(auth);
+app.use(auth);
 
-// app.use('/', userRouter);
-// app.use('/', movieRouter);
+app.use('/', userRouter);
+app.use('/', movieRouter);
 
 app.use(errorLogger);
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,86 +1,86 @@
-// /* eslint-disable consistent-return */
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
-// const User = require('../models/user');
-// const NotFoundError = require('../errors/NotFoundError');
-// // const BadRequestError = require('../errors/BadRequestError');
-// const UnauthorizedError = require('../errors/UnauthorizedError');
+/* eslint-disable consistent-return */
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const NotFoundError = require('../errors/NotFoundError');
+// const BadRequestError = require('../errors/BadRequestError');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
-// function getCurrentUser(req, res, next) {
-//   User.findById(req.user._id)
-//     .then((user) => {
-//       if (!user) {
-//         return Promise.reject(new NotFoundError('Такого пользователя не существует'));
-//       }
-//       return res.status(200).send(user);
-//     })
-//     .catch(next);
-// }
+function getCurrentUser(req, res, next) {
+  User.findById(req.user._id)
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new NotFoundError('Такого пользователя не существует'));
+      }
+      return res.status(200).send(user);
+    })
+    .catch(next);
+}
 
-// function updateProfile(req, res, next) {
-//   const { name, email } = req.body;
-//   User.findByIdAndUpdate(req.user._id, { name, email }, { new: true, runValidators: true })
-//     .then((user) => {
-//       if (!user) {
-//         return Promise.reject(new NotFoundError('Такого пользователя не существует'));
-//       }
-//       return res.status(200).send(user);
-//     })
-//     .catch(next);
-// }
+function updateProfile(req, res, next) {
+  const { name, email } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, email }, { new: true, runValidators: true })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new NotFoundError('Такого пользователя не существует'));
+      }
+      return res.status(200).send(user);
+    })
+    .catch(next);
+}
 
-// function createUser(req, res, next) {
-//   const {
-//     name, email, password,
-//   } = req.body;
+function createUser(req, res, next) {
+  const {
+    name, email, password,
+  } = req.body;
 
-//   bcrypt.hash(password, 10)
-//     .then((hash) => User.create({
-//       name, email, password: hash,
-//     }))
-//     .then((user) => res.status(200).send({
-//       name: user.name, about: user.about, avatar: user.avatar, email,
-//     }))
-//     .catch(next);
-// }
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name, email, password: hash,
+    }))
+    .then((user) => res.status(200).send({
+      name: user.name, about: user.about, avatar: user.avatar, email,
+    }))
+    .catch(next);
+}
 
-// function login(req, res, next) {
-//   res.send({ message: 'asdasddsa' });
-//   // let enteringUser;
+function login(req, res, next) {
+  res.send({ message: 'asdasddsa' });
+  // let enteringUser;
 
-//   // User.findOne({ email: req.body.email }).select('+password')
-//   //   .then((user) => {
-//   //     if (!user) {
-//   //       return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
-//   //     }
-//   //     enteringUser = user;
-//   //     return bcrypt.compare(req.body.password, user.password);
-//   //   })
-//   //   .then((matched) => {
-//   //     if (!matched) {
-//   //       return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
-//   //     }
-//   //     res.send({ message: 'asdasddsa' });
-//   //     // return Promise.reject(new UnauthorizedError([process.env, process.env.JWT_SECRET]));
-//   //     // const { JWT_SECRET } = process.env;
+  // User.findOne({ email: req.body.email }).select('+password')
+  //   .then((user) => {
+  //     if (!user) {
+  //       return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+  //     }
+  //     enteringUser = user;
+  //     return bcrypt.compare(req.body.password, user.password);
+  //   })
+  //   .then((matched) => {
+  //     if (!matched) {
+  //       return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+  //     }
+  //     res.send({ message: 'asdasddsa' });
+  //     // return Promise.reject(new UnauthorizedError([process.env, process.env.JWT_SECRET]));
+  //     // const { JWT_SECRET } = process.env;
 
-//   //     // const token = jwt.sign({ _id: enteringUser._id }, JWT_SECRET, { expiresIn: '7d' });
+  //     // const token = jwt.sign({ _id: enteringUser._id }, JWT_SECRET, { expiresIn: '7d' });
 
-//   //     // res.cookie('jwt', token, {
-//   //     //   maxAge: 3600000 * 24 * 7,
-//   //     //   httpOnly: true,
-//   //     //   sameSite: 'None',
-//   //     //   secure: true,
-//   //     // }).status(200).send({ _id: enteringUser._id });
-//   //   })
-//   //   .catch(next);
-// }
+  //     // res.cookie('jwt', token, {
+  //     //   maxAge: 3600000 * 24 * 7,
+  //     //   httpOnly: true,
+  //     //   sameSite: 'None',
+  //     //   secure: true,
+  //     // }).status(200).send({ _id: enteringUser._id });
+  //   })
+  //   .catch(next);
+}
 
-// function signOut(req, res, next) {
-//   res.clearCookie('jwt').status(200).send({ message: 'Вы вышли' })
-//     .catch(next);
-// }
+function signOut(req, res, next) {
+  res.clearCookie('jwt').status(200).send({ message: 'Вы вышли' })
+    .catch(next);
+}
 
-// module.exports = {
-//   getCurrentUser, updateProfile, createUser, login, signOut,
-// };
+module.exports = {
+  getCurrentUser, updateProfile, createUser, login, signOut,
+};
