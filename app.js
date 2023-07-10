56cd3076fe47f5f0982e65410953fbe55912dda3
evdokim-mellin <evdockimpyatkov.ru@yandex.ru>
2023-07-11 01:53:24 +0300

    feat: разрешить доступ CORS

--- app.js
+++ app.js
@@ -16,6 +16,31 @@ mongoose.connect(DATA_BASE);
 
 app.use(bodyParser.json());
 
+const allowedCors = [
+  'http://localhost:3000',
+  'http://api.movies-explorer-em.nomoredomains.work',
+  'https://api.movies-explorer-em.nomoredomains.work',
+];
+
+app.use((req, res, next) => {
+  const { method } = req;
+  const { origin } = req.headers;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+  }
+
+  next();
+});
+
 app.use(requestLogger);
 
 app.use(helmet());
