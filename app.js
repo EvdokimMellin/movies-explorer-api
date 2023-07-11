794fa11c147b744046a201b75e6aba686fdf5b39
evdokim-mellin <evdockimpyatkov.ru@yandex.ru>
2023-07-11 14:20:18 +0300

    fix: включить авторизацию обратно

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// const helmet = require('helmet');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
 app.use(requestLogger);
 
 // app.use(helmet());
-// app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 
 app.use('/', router);
 
