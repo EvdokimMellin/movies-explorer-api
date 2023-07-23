2ba24d4e7af8516a9727d452efcf6b75a9f9a65d
evdokim-mellin <evdockimpyatkov.ru@yandex.ru>
2023-07-23 21:48:36 +0300

    fix: отключить валидацию создания фильма

--- routes/movies.js
+++ routes/movies.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const {
   createMovie, getMovies, deleteMovie,
 } = require('../controllers/movies');
-const { createMovieValidator, deleteMovieValidator } = require('../utils/validators');
+// const { createMovieValidator, deleteMovieValidator } = require('../utils/validators');
+const { deleteMovieValidator } = require('../utils/validators');
 const NotFoundError = require('../errors/NotFoundError');
 
 router.get('/', getMovies);
-router.post('/', createMovieValidator, createMovie);
+// router.post('/', createMovieValidator, createMovie);
+router.post('/', createMovie);
 router.delete('/:movieId', deleteMovieValidator, deleteMovie);
 
 router.use((req, res, next) => {
